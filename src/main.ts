f4aa973e7bafb1f4b3a201ff84c7629642fca40d
EfosaE <osamwonyiefosa02@gmail.com>
2024-11-17 13:44:14 +0100

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Micromart')
+    .setDescription('The Micromart Api Description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
