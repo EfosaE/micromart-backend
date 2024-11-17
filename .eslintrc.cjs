2fbc2ec32619e95b276b280b171ddf113b4730b5
EfosaE <osamwonyiefosa02@gmail.com>
2024-11-17 16:06:07 +0100

    added eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser', // Use the TypeScript parser
+  parserOptions: {
+    project: './tsconfig.json', // Path to your TypeScript config file
+    sourceType: 'module',
+  },
+  env: {
+    node: true, // Indicates that the environment is Node.js, enabling Node.js global variables (e.g., `process`, `require`).
+    jest: true, // Enables Jest globals for linting tests, such as `describe`, `it`, `expect`.
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+};
